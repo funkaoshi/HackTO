c119af394c920005d99ad997d136abec257d45d4
Max Veytsman <maxim@ontoillogical.com>
2012-04-14 12:37:53 -0400

    playing a test file

--- jokeline.py
+++ jokeline.py
@@ -1,5 +1,7 @@
 import sqlite3
 from flask import abort, g, flash, make_response, redirect, request, render_template, url_for, Flask
+import soundcloud
+
 
 
 # configuration
@@ -7,12 +9,15 @@ from flask import abort, g, flash, make_response, redirect, request, render_temp
 DATABASE = 'jokes.db'
 SECRET_KEY = 'Y\xf6\xf2j\xc9\xc5\xbc\xde{\xae\x9a\xc8\x8dZ0\x9e\x14\xb6\x90\xd7\x02\x03\xf0\x1a'
 DEBUG = True
+SOUNDCLOUD_ID = '790496b735a696eb3261822846618016'
+SOUND_CLOUD_SECRET = '838cb0dd9b5bcb39b4036ae222a1c124'
 
 
 # create application
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+sc_client = soundcloud.Client(client_id=SOUNDCLOUD_ID)
 
 
 # Poor Man's 'ORM' with SQLite3
@@ -53,7 +58,9 @@ def index():
 #Twilio stuff
 @app.route('/twilio/voice', methods=['GET', 'POST'])
 def twilio_voice():
-    resp = make_response(render_template("twilio/voice.xml"))
+    track = sc_client.get('/resolve', url='http://soundcloud.com/jokeline/untitled-recording')
+    url = track.download_url
+    resp = make_response(render_template("twilio/voice.xml", joke_url=url))
     resp.headers['Content-Type'] = 'text/xml'
     return resp
 
